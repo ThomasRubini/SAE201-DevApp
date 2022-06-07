69e0c628e94be801ccb85054e8b33f07ad9ed866
SIMAILA Djalim <djalimS.pro@outlook.fr>
2022-06-07 21:34:57 +0200

    bouton login et menu de login fonctionnels, mais avec les nouveaux fichier added to git

---
+++ src/main/java/fr/univ_amu/iut/loginPrompt/Controller.java
@@ -0,0 +1,33 @@
+
+package fr.univ_amu.iut.loginPrompt;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Button log;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        
+        EventHandler<ActionEvent> handleLogin = event ->{
+            System.out.println(password.getText());
+        };
+        log.setOnAction(handleLogin);
+        
+    }
+
+}
